2985e4b0fa8d85078ed6b7a0070f85f3cf703423
Ashok Babu Marthu <ashokmarthu999@gmail.com>
2024-11-20 17:15:38 +0530

    Enhance Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <div>For better experience,download the Swiggy app now</div>
+      <div>For better experience,download the Swigaato app now</div>
       <div>
         <img
           src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png"
